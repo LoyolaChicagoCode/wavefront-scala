8ebd7afde2c5cfc80a4516a1dbc535764b272889
Konstantin Läufer <klaeufer@gmail.com>
2021-08-24 11:34:39 -0500

    automatic rewrite to Scala 3 syntax
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/Wavefront.scala
+++ src/main/scala/Wavefront.scala
@@ -3,14 +3,14 @@ package edu.luc.etl.scala.wavefront
 object Main {
 
   def main(args: Array[String]) = {
-    if (args.length < 1) {
+    if args.length < 1 then {
       Console.err.println("usage: Main n")
       System.exit(1)
     }
     val n = args(0).toInt
     val w = wavefront(n)
-    for (i <- 0 until n) {
-      for (j <- 0 until n) {
+    for i <- 0 until n do {
+      for j <- 0 until n do {
         print(w(i)(j))
         print(" ")
       }
@@ -20,9 +20,9 @@ object Main {
 
   def wavefront(n: Int) = {
     lazy val a: LazyList[LazyList[Int]] = LazyList.tabulate(n, n)((i, j) =>
-      if (i == 0)
+      if i == 0 then
         j
-      else if (j == 0)
+      else if j == 0 then
         i
       else
         a(i)(j - 1) + a(i - 1)(j - 1) + a(i - 1)(j))

7493d7217a30920f05d59c0d2a749f25bb6fe196
Konstantin Läufer <laufer@cs.luc.edu>
2022-09-17 22:13:04 -0500

    updated examples to use fewer braces
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/Wavefront.scala
+++ src/main/scala/Wavefront.scala
@@ -1,24 +1,20 @@
 package edu.luc.etl.scala.wavefront
 
-object Main {
+object Main:
 
-  def main(args: Array[String]) = {
-    if args.length < 1 then {
+  def main(args: Array[String]) =
+    if args.length < 1 then
       Console.err.println("usage: Main n")
       System.exit(1)
-    }
     val n = args(0).toInt
     val w = wavefront(n)
-    for i <- 0 until n do {
-      for j <- 0 until n do {
+    for i <- 0 until n do
+      for j <- 0 until n do
         print(w(i)(j))
         print(" ")
-      }
       println()
-    }
-  }
 
-  def wavefront(n: Int) = {
+  def wavefront(n: Int) =
     lazy val a: LazyList[LazyList[Int]] = LazyList.tabulate(n, n)((i, j) =>
       if i == 0 then
         j
@@ -27,5 +23,5 @@ object Main {
       else
         a(i)(j - 1) + a(i - 1)(j - 1) + a(i - 1)(j))
     a
-  }
-}
+
+end Main
